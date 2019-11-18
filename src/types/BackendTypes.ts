734e0e15e88fc1ad5192ef6e50b23c221e1203dc
egeokt <egeokten@gmail.com>
2019-11-18 11:41:07 -0800

    add types for directory parsing

---
+++ src/types/BackendTypes.ts
@@ -0,0 +1,14 @@
+export interface DirectoryNode {
+    name: string;
+    path: string;
+    parent: DirectoryNode | null ;  // head if this is null
+    children: DirectoryNode[];
+    freshnessScore: number;
+}
+
+export interface DirectoryNodeLeaf{
+    name: string;
+    path: string;
+    parent: DirectoryNode;
+    freshnessScore: number;
+}
