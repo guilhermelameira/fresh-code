abb5f39c79d61e534c8197ad2358983db1ea59a4
Prayansh Srivastava <prayansh@gmail.com>
2019-11-19 21:11:47 -0800

    fix freshness

--- src/Main.ts
+++ src/Main.ts
@@ -1,24 +1,23 @@
-import {buildDirectoryTree, getRepoFiles, printDirectory, removeClone} from "./backend/FreshnessCalculator";
-import {runShellCommand} from "./backend/ShellCommander";
+import {buildDirectoryTree, getRepoFiles} from "./backend/FreshnessCalculator";
 import * as path from "path";
-import {getFreshness, parseFile} from "./backend/Parser";
+import {getFreshness, parseFile, printFreshness} from "./backend/Parser";
 
-export const ROOT_DIR = path.join(__dirname, "..");
-export const REPO_DIR = path.join(ROOT_DIR, "repo");
+export const PROJECT_ROOT_PATH = path.join(__dirname, "..");
+export const REPO_DIR = path.join(PROJECT_ROOT_PATH, "repo");
 
 class Main {
     public main() {
         // For now clone a repo into the root project folder with the folder name repo
         // `git clone <link> repo` in project root folder
-        //  `git ls-files | xargs -I{} sh -c 'git blame -t -C -M -- $1 > "$1.adat"' -- {}` will generate the git blame files
-        // let files = getRepoFiles(REPO_DIR);
-        // console.log(files)
-        // let root = buildDirectoryTree(files, "");
-        let readme = parseFile("README.md");
-        let freshness = getFreshness(readme, 1573603200);
-        console.log('freshness', freshness)
-        // printDirectory(root, 0);
-        // console.log(root)
+        //  `git ls-files | xargs -I{} sh -c 'git blame -t -C -M -e -w-- $1 > "$1.adat"' -- {}` will generate the git blame files
+
+        let files = getRepoFiles(REPO_DIR);
+        let root = buildDirectoryTree(files, "");
+        printFreshness(root, 0, 1571768680)
+
+        // let file = parseFile("src/parser/PlaintextParser.ts");
+        // let freshness = getFreshness(file, 1572480000);
+        // console.log('freshness', freshness)
     }
 }
 
--- src/backend/Parser.ts
+++ src/backend/Parser.ts
@@ -3,21 +3,27 @@ import * as fs from "fs";
 import * as path from "path";
 import {REPO_DIR} from "../Main";
 
-function f(root: DirectoryNode) {
-
+export function printFreshness(root: DirectoryNode, tabs: number, refTime: number) {
+    if (root.children.length === 0) {
+        let freshness = getFreshness(parseFile(root.path), refTime);
+        console.log(`${"\t".repeat(tabs)}${root.path}\t${freshness}`)
+    } else {
+        for (let child of root.children) {
+            printFreshness(child, tabs + 1, refTime);
+        }
+    }
 }
 
-export function getFreshness(file: FileBlame, refTime: number) {
+export function getFreshness(file: FileBlame, refTime: number): number {
     let val: number = 0.0;
     file.blameData.forEach((b: BlameData) => {
-        // TODO fix
-        console.log(Math.exp(-0.03 * (refTime - b.timestamp)))
-        val += 100 * Math.exp(-0.03 * (refTime - b.timestamp));
+        const timeVal = (refTime - b.timestamp) / 604800;
+        val += 100 * Math.exp(-0.03 * timeVal);
     });
     return val / file.blameData.length
 }
 
-export function getOwnership(file: FileBlame) {
+export function getOwnership(file: FileBlame): Map<string, number> {
     let val: Map<string, number> = new Map<string, number>();
     file.blameData.forEach((b: BlameData) => {
         let cur = val.get(b.author) || 0;
@@ -29,15 +35,16 @@ export function getOwnership(file: FileBlame) {
     return val;
 }
 
-export function parseFile(filePath: string) {
+export function parseFile(filePath: string): FileBlame {
     try {
         const file = fs.readFileSync(path.join(REPO_DIR, filePath + ".adat")).toString('utf-8')
         let lines: string[] = file.trim().split('\n');
         const blameData: BlameData[] = lines.map((l: string) => {
             let tokens = l.match(/\S+/g) as string[];
+            // console.log("TOKENS", tokens[0], tokens[1], tokens[2]);
             const commitHash = tokens[0];
-            const author = tokens[1];
-            const timestamp = parseInt(tokens[2], 10);
+            const author = tokens[2];
+            const timestamp = parseInt(tokens[3], 10);
             return {
                 commitHash,
                 author,
