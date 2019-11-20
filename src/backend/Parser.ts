8690022eb32cf16669ec3ddb9b5597587f421fc7
Prayansh Srivastava <prayansh@gmail.com>
2019-11-19 23:24:24 -0800

    fix formatting

--- src/backend/Parser.ts
+++ src/backend/Parser.ts
@@ -61,17 +61,3 @@ export function parseFile(filePath: string): FileBlame {
         return {} as FileBlame
     }
 }
-
-
-export const countLines = function (filePath: string, callback: (e: any, n: number) => void) {
-    // function copied from http://stackoverflow.com/questions/12453057/node-js-count-the-number-of-lines-in-a-file
-    // with very few modifications
-    let i;
-    let count = 0;
-    fs.createReadStream(filePath)
-        .on('error', e => callback(e, 0))
-        .on('data', chunk => {
-            for (i=0; i < chunk.length; ++i) if (chunk[i] == 10) count++;
-        })
-        .on('end', () => callback(null, count));
-};
