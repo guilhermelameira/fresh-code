65e8620b107ab5d9fb144e2c1654032e2a54bd5e
Prayansh Srivastava <prayansh@gmail.com>
2019-11-21 22:03:07 -0800

    add more automation

--- src/Main.ts
+++ src/Main.ts
@@ -7,30 +7,33 @@ import {
 import * as fs from "fs";
 import * as path from "path";
 import {getFreshness, getOwnership, parseFile} from "./backend/Parser";
+import {runShellCommand} from "./backend/ShellCommander";
 
 export const PROJECT_ROOT_PATH = path.join(__dirname, "..");
 export const REPO_DIR = path.join(PROJECT_ROOT_PATH, "repo");
+const REPO_LINK = "git@github.com:segmentio/analytics-ios.git";
 
 class Main {
     public main() {
         // For now clone a repo into the root project folder with the folder name repo
         // `git clone <link> repo` in project root folder
+        runShellCommand(`rm -rf repo`, PROJECT_ROOT_PATH);
+        runShellCommand(`git clone ${REPO_LINK} repo`, PROJECT_ROOT_PATH);
         //  `git ls-files | xargs -I{} sh -c 'git blame -t -C -M -f -e -w -- $1 > "$1.adat"' -- {}` will generate the git blame files
+        runShellCommand(`git ls-files | xargs -I{} sh -c 'git blame -t -C -M -f -e -w -- $1 > "$1.adat"' -- {}`, REPO_DIR)
 
         //repoName = cloneRepo('https://github.com/guilhermelameira/fresh-code.git');
+        // git log --pretty=format:%ct | tail -1 [First time]
+        // git log --pretty=format:%ct | head -1 [Latest time]
+        const firstTimestampStr = runShellCommand("git log --pretty=format:%ct | tail -1", REPO_DIR);
+        const latestTimestampStr = runShellCommand("git log --pretty=format:%ct | head -1", REPO_DIR);
+        const firstTimestamp = parseInt(firstTimestampStr);
+        const latestTimestamp = parseInt(latestTimestampStr);
         let files = getRepoFiles(REPO_DIR);
         let root = buildDirectoryTree(files, "repo");
-        calculateFreshnessForFiles(root, 1574280919);
+        calculateFreshnessForFiles(root, latestTimestamp);
         let graph = generateGraphData(root);
-        // console.log(graph);
         this.writeToFile(graph);
-        // removeClone();
-
-        // let ownership = getOwnership(parseFile("package.json"), 1571768680);
-        // console.log(ownership)
-        // ownership = getOwnership(parseFile("src/parser/RichTextParser.ts"), 1571768680);
-        // console.log(ownership)
-
     }
 
     public writeToFile(obj: any) {
